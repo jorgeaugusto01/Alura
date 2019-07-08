49fce081ca65b39c71e09a2c91f4510eae254fd9
jorgeaugusto01 <jorgeaugusto01@gmail.com>
2019-07-08 18:13:17 -0300

    curso

--- Django/connectedin/usuarios/urls.py
+++ Django/connectedin/usuarios/urls.py
@@ -1,8 +1,7 @@
 from django.urls import path
-from views import RegistrarUsuarioView
 
 from . import views
 
 urlpatterns = [
-    path('registrar/', RegistrarUsuarioView.as_view(), name='registrar'),
+    path('registrar/', views.RegistrarUsuarioView.as_view(), name='registrar'),
 ]
